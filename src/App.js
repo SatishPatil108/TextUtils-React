18725d0c441b0c6289f97a437d88738ad8f4e04f
Satish Patil <patilsatish7847@gmail.com>
2022-07-04 12:21:13 +0530

    Escape

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import "./App.css";
-// import About from "./components/About";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import React, { useState } from "react";
 import Alert from "./components/Alert";
 
 
-// import {
-//   // BrowserRouter,
-//   Routes,
-//   Route,
-//   // Link
-// } from "react-router-dom";
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
 
 
 
 function App() {
-  const [mode, setMode] = useState('light');//whethere darkMode is enable or not
+  const [mode] = useState('light');//whethere darkMode is enable or not
 
-  // const [colmode, setColor] = useState(null);
-
+ 
   const [alert, setAlert] = useState(null);
 
   const showAlert = (message, type) => {
@@ -33,45 +30,22 @@ function App() {
     }, 2000);
   }
 
-
-  const toggleMode = () => {
-    if (mode === 'light') {
-      setMode('dark');
-      document.body.style.backgroundColor = '#042743';
-      showAlert("Dark mode has been enabled", "success");
-      document.title = "TextUtils - Dark Mode";
-
-      // setInterval(() => {
-      //   document.title = "TextUtils is Amazing Mode";
-      // }, 2000);
-
-      // setInterval(() => {
-      //   document.title = "TextUtils Install Now";
-      // }, 1500);
-    }
-    else {
-      setMode('light');
-      document.body.style.backgroundColor = 'white';
-      showAlert("Light mode has been enabled", "success");
-      document.title = "TextUtils - Light Mode";
-    }
+  const removeBodyClasses=()=>{
+      document.body.classList.remove('bg-dark')
+      document.body.classList.remove('bg-light')
+      document.body.classList.remove('bg-warning')
+      document.body.classList.remove('bg-primary')
+      document.body.classList.remove('bg-danger')
+      document.body.classList.remove('bg-success')
   }
 
-  // const colorMode = () => {
-  //   if (colmode === onclick){
-  //     setColor('green');
-  //     document.body.style.backgroundColor = '#198754';
-  //     showAlert("Green mode has been enabled", "success");
-  //     document.title = "TextUtils - Green Mode";
-  //   }
-  //   else {
-  //     setColor('red');
-  //     document.body.style.backgroundColor = '#dc3545';
-  //     showAlert("Red mode has been enabled", "success");
-  //     document.title = "TextUtils - Red Mode";
-  //   }
-  // }
-
+  const toggleMode = (cls) => {
+    removeBodyClasses();
+    document.body.classList.add('bg-'+cls);
+  }
+      
+    
+  
   return (
     <>
       {/* <Navbar title ="TextUtils" aboutText = "About TextUtils" /> */}
@@ -81,17 +55,16 @@ function App() {
       <Alert alert={alert} />
 
       <div className="container my-3">
-        {/* <Routes> */}
+        <Routes>
           {/* /users  --> Components 1
           /users/home  --> Components 2 */}
-          {/* <Route exact path="/about" element={<About />} /> */}
-          {/* < Route exact path="/" */}
-          {/* element= */}
-          {<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />}
-           {/* /> */}
-        {/* </Routes> */}
-      
-      </div>
+          <Route exact path="/about" element={<About mode={mode} />} />
+          < Route exact path="/"
+           element= 
+          {<TextForm showAlert={showAlert} heading="TextUtils - Word Counter, Character Counter , Remove Extra Spaces" mode={mode} />}
+           /> 
+        </Routes>
+       </div> 
 
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -27,9 +27,7 @@ export default function TextForm(props) {
   };
 
   const handleCopy = () => {
-    var text = document.getElementById("myBox");
-    text.select();
-    navigator.clipboard.writeText(text.value);
+    navigator.clipboard.writeText(text);
     props.showAlert("Copy to Clipboard!","success");
   }
 
@@ -44,44 +42,44 @@ export default function TextForm(props) {
 
   return (
     <>
-      <div className="container" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
-        <h1>{props.heading} </h1>
+      <div className="container"  style={{ color: props.mode === 'light' ? 'black' : '#042743' }}>
+        <h1 className='mb-4'>{props.heading} </h1>
         <div className="mb-3">
           <textarea
             className="form-control"
             value={text}
             onChange={handleOnChange}
             style={{
-              backgroundColor: props.mode === 'dark' ? 'grey' : 'white',
+              backgroundColor: props.mode === 'dark' ? '#13466e' : 'white',
               color: props.mode === 'dark' ? 'white' : '#042743'
             }}
             id="myBox"
             rows="8"
           ></textarea>
         </div>
-        <button className="btn btn-primary mx-1" onClick={handleUpClick}>
+        <button disabled={text.length===0} className="btn btn-primary my-1 mx-1" onClick={handleUpClick}>
           Convert to Uppercase
         </button>
-        <button className="btn btn-primary mx-1" onClick={handleLoClick}>
+        <button disabled={text.length===0} className="btn btn-primary my-1 mx-1" onClick={handleLoClick}>
           Convert to LowerCase
         </button>
-        <button className="btn btn-primary mx-1" onClick={handleClearClick}>
+        <button disabled={text.length===0} className="btn btn-primary my-1 mx-1" onClick={handleClearClick}>
           Cleartext
         </button>
-        <button className="btn btn-primary mx-1" onClick={handleCopy}>
+        <button disabled={text.length===0} className="btn btn-primary my-1 mx-1" onClick={handleCopy}>
           Copytext
         </button>
-        <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>
+        <button disabled={text.length===0} className="btn btn-primary my-1 mx-1" onClick={handleExtraSpaces}>
           RemoveExtra Spaces
         </button>
       </div>
 
-      <div className="container my-3" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
+      <div className="container my-3"  style={{ color: props.mode === 'light' ? 'black' : '#042743' }}>
         <h1>Your text summary</h1>
-        <p>{text.split(" ").length} words and {text.length} characters</p>
-        <p>{0.008 * text.split(" ").length} Minutes read </p>
+        <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read </p>
         <h2>Preview</h2>
-        <p>{text.length>0? text:"Enter something to textabove to preview its here"}</p>
+        <p>{text.length>0? text:"Nothing to Preview!"}</p>
       </div>
     </>
   );
