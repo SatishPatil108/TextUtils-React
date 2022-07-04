7f884a500729a655500c4d0ba2e48f3f7c367387
Satish Patil <patilsatish7847@gmail.com>
2022-07-04 22:11:14 +0530

    Updates

---
+++ precache-manifest.07041e47de8074ff8dd98fab5e69aec0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e3c4de3c87aceb34ce1",
+    "url": "/TextUtils-React/static/css/main.16c62769.chunk.css"
+  },
+  {
+    "revision": "6e3c4de3c87aceb34ce1",
+    "url": "/TextUtils-React/static/js/main.6e3c4de3.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/TextUtils-React/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "a2a9d58f5d82f850426c",
+    "url": "/TextUtils-React/static/js/2.a2a9d58f.chunk.js"
+  },
+  {
+    "revision": "2499cddb344818b4039d",
+    "url": "/TextUtils-React/static/js/runtime~main.2499cddb.js"
+  },
+  {
+    "revision": "24164586061c28ecc1b399afb2ec146c",
+    "url": "/TextUtils-React/index.html"
+  }
+];
